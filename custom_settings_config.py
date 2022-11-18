d78afd35e256c72b8205d43cd055a9ae6425a66f
Pratik <steveparmar6nov2011@gmail.com>
2022-11-18 18:14:37 +0530

    Add custom settings for scrapy

---
+++ custom_settings_config.py
@@ -0,0 +1,9 @@
+
+custom_settings = dict(
+        DOWNLOAD_HANDLERS = {
+            "http": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+            "https": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+        },
+        TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+        ZYTE_API_KEY = "YOUR ZYTE API KEY HERE"
+)
