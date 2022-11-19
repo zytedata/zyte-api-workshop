b4dbfff0f276053649427cb99063f9631ec3aa37
Pratik <steveparmar6nov2011@gmail.com>
2022-11-19 06:56:05 +0530

    Add browser action generic spider

---
+++ browser_action_generic.py
@@ -0,0 +1,35 @@
+from scrapy import Request, Spider
+from base64 import b64decode, decodebytes
+import custom_settings_config
+
+class BrowserActionGenericSpider(Spider):
+    name = 'browser_action_generic'
+
+    custom_settings = custom_settings_config.custom_settings
+
+    def start_requests(self):
+        yield Request(
+            "https://quotes.toscrape.com/scroll",
+            meta={
+                "zyte_api_automap": {
+                    "browserHtml": True,
+                    "screenshot": True,
+                    "screenshotOptions": {
+                        "fullPage": True
+                        },
+                    "actions": [{
+                            "action": "scrollBottom",
+                        }
+                    ]
+                }
+            }
+        )
+        
+    def parse(self, response):
+        quote_count = len(response.css(".quote"))
+        print("Quote Count: ", quote_count)
+
+        screenshot: bytes = b64decode(response.raw_api_response["screenshot"]) # decode base64 response
+        with open("output_quote.jpg", "wb") as fh: 
+            fh.write(screenshot) # write bytes to the output.jpg file
+
